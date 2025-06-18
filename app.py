a40e16dca8f1ad484c95c9d8c06edea09cbb7970
adenadoume <adenadoume@gmail.com>
2025-06-18 22:54:56 +0300

    Update app.py
    railway

--- app.py
+++ app.py
@@ -42,3 +42,10 @@ def save(record_id):
 
 if __name__ == "__main__":
 	app.run(debug=True)
+	
+	
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+	
+	
+
